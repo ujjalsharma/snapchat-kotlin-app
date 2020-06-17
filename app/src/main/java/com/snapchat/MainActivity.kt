2e4f24aa60b9b1dcae8e2e95312955b75516ce5f
ujjalsharma <ujjalsharma81@gmail.com>
2020-06-17 10:37:25 +0530

    realtime database resolved

--- app/src/main/java/com/snapchat/MainActivity.kt
+++ app/src/main/java/com/snapchat/MainActivity.kt
@@ -7,13 +7,18 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     var emailEditText: EditText? = null
     var passEditText: EditText? = null
     val mAuth = FirebaseAuth.getInstance()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
